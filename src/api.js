36d288fe54f97c4499b645c18c3ee2ea2c2b60b7
Matheus <matheus.miranda@hotmail.com>
2023-05-30 21:39:36 -0300

    upload

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+    //baseURL: apiUrl,
+    baseURL: 'https://site-pessoal-api-0f6p.onrender.com/api'
+    //baseURL: 'https://json-server-ex-gq2u.onrender.com',
+    //baseURL: 'http://localhost:5000',
+    //baseURL: 'https://site-pessoal-api-0f6p.onrender.com'
+});
+
+export default api;
+
